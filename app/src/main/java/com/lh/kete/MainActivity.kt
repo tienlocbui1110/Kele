1ff4ed2eaf867265acd8b043d75b8d86c2993508
Loc <tienlocbui1110@gmail.com>
2019-03-13 23:39:37 +0700

    init

---
+++ app/src/main/java/com/lh/kete/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.lh.kete
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
